522819f8e4ae255ea2c9ca40371a5402332625d7
Richard Boulton <richard@tartarus.org>
2010-03-19 16:51:13 +0000

    Add parameter validation

--- service.py
+++ service.py
@@ -5,17 +5,19 @@ import search
 
 @wsgiwapi.jsonreturning
 @wsgiwapi.allow_GETHEAD
+@wsgiwapi.param("loc", 1, 1, None, None,
+                "The location to search for registry offices near.")
 def find_registry(request):
-    loc = request.GET['location']
+    loc = request.GET['loc']
     res = search.search('regoff.db', loc[0])
     return res
 
 def main():
     app = wsgiwapi.make_application({
-        'registry/find': find_registry,
-    })
+        'registries': find_registry,
+    }, autodoc='doc')
 
-    server = wsgiwapi.make_server(app, ('0.0.0.0', 8080))
+    server = wsgiwapi.make_server(app, ('0.0.0.0', 10010))
     try:
 	server.start()
     except KeyboardInterrupt:
