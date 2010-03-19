b6e353f588c5eaf65280601be6ebea91a2928919
Richard Boulton <richard@tartarus.org>
2010-03-19 15:55:42 +0000

    Add england and wales data

--- add_locs.py
+++ add_locs.py
@@ -2,17 +2,24 @@
 
 import geoloc
 import parse_groni
+import parse_groenwales
 import time
 import json
 
+def parseall():
+    for item in parse_groni.get():
+        yield item
+    for item in parse_groenwales.get():
+        yield item
+
 def get():
     results = []
-    for c, result in enumerate(parse_groni.get()):
+    for c, result in enumerate(parseall()):
 	coord = geoloc.get(result['address'], result['postcode'])
 	result['longlat'] = coord
         results.append(result)
     	print c, result
-	time.sleep(1)
+	time.sleep(0.5)
     return results
 
 if __name__ == '__main__':
