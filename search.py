a0e4feb727cd7611e3f2d5fef681b4600c5bf59c
Richard Boulton <richard@tartarus.org>
2010-03-20 11:45:48 +0000

    Add maxhits support, and documentation

--- search.py
+++ search.py
@@ -7,19 +7,28 @@ try:
 except ImportError:
     import json
 
-def search(indexpath, loc1, loc2=None):
+def search(indexpath, loc1, loc2=None, maxhits=10):
     loc = geoloc.get(loc1, loc2)
     if loc[0] is None:
         return None
     loc = loc[1] + " " + loc[0]
     conn = xappy.SearchConnection(indexpath)
-    results = []
-    for result in conn.query_distance('location', loc).search(0, 10):
+    registries = []
+    for result in conn.query_distance('location', loc).search(0, maxhits):
         data = json.loads(result.data['data'][0])
         dm = float(result.get_distance('location', loc)) / 1609.344
         data['dist_miles'] = "%.1f" % dm
-        results.append(data)
-    return results
+        registries.append(data)
+    return loc, registries
+
+def all(indexpath):
+    conn = xappy.SearchConnection(indexpath)
+    registries = []
+    for result in conn.iter_documents():
+        data = json.loads(result.data['data'][0])
+        registries.append(data)
+    return registries
 
 if __name__ == '__main__':
     print search('regoff.db', "BT23")
+    print all('regoff.db')
--- service.py
+++ service.py
@@ -5,23 +5,123 @@ import search
 
 @wsgiwapi.jsonreturning
 @wsgiwapi.allow_GETHEAD
-@wsgiwapi.param("loc", 1, 1, None, None,
+@wsgiwapi.param("loc", 1, 1, None, [''],
                 "The location to search for registry offices near.")
+@wsgiwapi.param("maxhits", 1, 1, '[0-9]+', ['10'],
+                "The maximum number of offices to return.")
 def find_registry(request):
-    loc = request.GET['loc']
-    res = search.search('regoff.db', loc[0])
+    loc = request.params['loc'][0]
+    maxhits = int(request.params['maxhits'][0])
+    res = {}
+    if loc == '':
+        res['registries'] = search.all('regoff.db')[:maxhits]
+    else:
+        res['query_location'] = loc
+        res['query_latlong'], res['registries'] = \
+            search.search('regoff.db', loc, maxhits=maxhits)
     return res
+
+def docs(request):
+    tmpl = '''
+<html>
+  <head>
+    <title>Registry Office Locator, API documentation</title>
+  </head>
+  <body>
+    <h1>Registry Office Locator, API documentation</h1>
+    <h2>Find offices</h2>
+    <p>
+      <em>Base url</em>:
+        <ul>
+          <li>
+            <code>http://tartarus.org/richard/registries/find</code>
+          </li>
+        </ul>
+    </p>
+    <p>
+      <em>Parameters</em>:
+      <ul>
+        <li>
+          <code>loc</code>: The location to centre the search on.  May be an
+          address or a postcode.  If omitted, all registry offices will be
+          returned (subject to the <code>maxhits</code> parameter).
+        </li>
+        <li>
+          <code>maxhits</code>: The maximum number of matching centres to
+          return.  If omitted, defaults to 10.
+        </li>
+      </ul>
+    </p>
+    <p>
+      <em>Results</em>:
+      Returns a JSON object, with the following attributes:
+      <ul>
+        <li>
+          <q><code>query_location</code><q>: The location that was searched for
+          (ie, the loc parameter supplied to the search).  Missing if no
+          location was specified.
+        </li><li>
+          <q><code>query_latlong</code></q>: The latitude and longitude
+          determined for the location (in decimal, separated by a space).
+          Missing if no location was specified.
+        </li><li>
+          <q><code>registries</code></q>: A list of registries, in order of
+          distance from the query location (unless no location specified, in
+          which case the order is undefined).  Each registry is a JSON object,
+          with the following attributes:
+          <ul><li>
+            <q><code>name</code></q>: The name of the registry office.
+           </li><li>
+            <q><code>addresss</code></q>: The address of the registry office.
+           </li><li>
+            <q><code>postcode</code></q>: The postcode of the registry office.
+           </li><li>
+            <q><code>longlat</code></q>: The location of the registry office,
+            as a list of two items; longitude first.
+           </li><li>
+            <q><code>dist_miles</code></q>: Distance from the query location in
+            miles: omitted if no location was specified.
+          </li></ul>
+          Other fields may be present, but aren't for all registry offices.
+        </li>
+      </ul>
+    </p>
+  </body>
+</html>
+'''.strip()
+    return wsgiwapi.Response(tmpl, content_type='text/html')
+
+def frontpage(request):
+    tmpl = '''
+<html>
+  <head>
+    <title>Registry Office Locator</title>
+  </head>
+  <body>
+    <ul>
+    <li>
+    <a href="find">Find offices API</a>
+    </li>
+    <li>
+    <a href="docs">API documentation</a>
+    </li></ul>
+  </body>
+</html>
+'''.strip()
+    return wsgiwapi.Response(tmpl, content_type='text/html')
 
 def main():
     app = wsgiwapi.make_application({
-        'registries': find_registry,
-    }, autodoc='doc')
+        'find': find_registry,
+        'docs': docs,
+        '': frontpage,
+    })
 
     server = wsgiwapi.make_server(app, ('0.0.0.0', 10010))
     try:
-	server.start()
+        server.start()
     except KeyboardInterrupt:
-	server.stop()
+        server.stop()
 
 if __name__ == '__main__':
     main()
